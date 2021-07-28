303b79ff5a11d91cccf0c0155a0af2629acb1f7b
Dennis Lieu <lieudennis7@gmail.com>
2021-07-28 16:29:38 -0600

    Added basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import List from "./List";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Todo List</h1>
+      <main>
+        <form>
+          <input type="text"></input>
+          <button type="submit">Add</button>
+        </form>
+        <List />
+      </main>
+    </>
   );
 }
 
