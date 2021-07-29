2df606f20c3cb0f9da4b250c94c816741b92672a
Dennis Lieu <lieudennis7@gmail.com>
2021-07-29 15:22:13 -0600

    Added feature: can now add things to the list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,36 @@ import List from "./List";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+  const [inputText, setInputText] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputText) {
+      // Show error
+    } else {
+      const newItem = { id: new Date().getTime().toString(), name: inputText };
+      setList([...list, newItem]);
+    }
+    setInputText("");
+  };
 
   return (
     <>
       <h1>Todo List</h1>
       <main>
-        <form>
-          <input type="text"></input>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add an Item"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          ></input>
           <button type="submit">Add</button>
         </form>
-        <List />
+        <List list={list} />
       </main>
     </>
   );
--- src/List.js
+++ src/List.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const List = () => {
-  return <div>List Here</div>;
+const List = ({ list }) => {
+  return (
+    <section>
+      {list.map((item) => {
+        const { id, name } = item;
+        return <p key={id}>{name}</p>;
+      })}
+    </section>
+  );
 };
 
 export default List;
